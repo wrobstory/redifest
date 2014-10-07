9081fc09a234aea7159a9f1856772722733f258d
Rob Story <rob@simple.com>
2014-10-07 14:59:02 -0700

    ENH: Add redifest.py, update README

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-redifest
-========
+### Redifest: Generate a Redshift .manifest file for a given S3 bucket/folder
 
-Generate a Redshift .manifest file for a given S3 bucket
+Does what it says on the tin: Generates a [Redshift .manifest file](http://docs.aws.amazon.com/redshift/latest/dg/loading-data-files-using-manifest.html) given a list of S3 buckets. Can write said manifest to file, or back to S3.
+
+API
+___
+
+Create a manifest generator with your AWS creds:
+
+```python
+>>> gen = ManifestGenerator('aws_access_key_id', 'aws_secret_access_key')
+```
+
+If following the [boto convention](http://boto.readthedocs.org/en/latest/s3_tut.html#creating-a-connection) for creds in env variables, you do not need to pass them in.
+
+Generate a manifest:
+```python
+>>> manifest = gen.generate_manifest(['mybucket/folder1/folder2', 'mybucket/folder3'])
+
+{'entries': [{'mandatory': True,
+              'url': u's3://mybucket/folder1/folder2/foo.json'},
+             {'mandatory': True,
+              'url': u's3://mybucket/folder1/folder2/bar.json'},
+             {'mandatory': True,
+               'url': u's3://mybucket/folder3/bar.json'}]}
+```
+
+You can provide an optional path to write the manfifest back to S3 as part of the call
+to generate the manifest:
+```python
+>>> gen.generate_manifest(['mybucket/folder1/folder2', 'mybucket/folder3'],
+                          target='mybucket/manifest_files/qux.manifest')
+```
+
+Or write the dict you generated in `generate_manifest`:
+```python
+>>> gen.write_manifest(manifest, 'mybucket/manifest_files/qux.manifest')
+```
+
+You can also write to file:
+```python
+>>> gen.generate_manifest(['mybucket/folder1/folder2', 'mybucket/folder3'],
+                          path='qux.manifest')
+```
---
+++ redifest.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Redifest: Redshift Manifest Generator
+---------------------------
+
+Generate a .manifest file for given list of S3 buckets and PUT it to
+a given S3 bucket
+
+"""
+from __future__ import print_function
+import json
+
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+class ManifestGenerator(object):
+
+    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None):
+        """
+        The Manifest Generator generates manifests generatively.
+
+        The aws keys are not required if you have environmental params set
+        for boto to pick up:
+        http://boto.readthedocs.org/en/latest/s3_tut.html#creating-a-connection
+
+        Parameters
+        ----------
+        aws_access_key_id: str
+        aws_secret_access_key: str
+        """
+        if aws_access_key_id and aws_secret_access_key:
+            self.conn = S3Connection()
+        else:
+            self.conn = S3Connection(aws_access_key_id, aws_secret_access_key)
+
+    def _get_bucket_and_key(self, path):
+        """Get top-level bucket and nested key path"""
+        if '/' in path:
+            parts = path.split('/')
+            buckpath = parts[0]
+            keypath = '/'.join(parts[1:])
+        else:
+            buckpath, keypath = path, ""
+        return buckpath, keypath
+
+    def generate_manifest(self, buckets, mandatory=True, path=None,
+                          target=None):
+        """
+        Given a list of S3 buckets, generate a .manifest file (JSON format).
+
+        You can pass in folders to the bucket argument, and this will
+        only grab keys in those folders.
+
+        Parameters
+        ----------
+        buckets: list
+        mandatory: boolean
+            The mandatory flag indicates whether the Redshift COPY should
+            terminate if the file does not exist.
+        path: str, default None
+            Optional file path to write manifest file.
+        target: str, default None
+            Optional S3 path to write manifest file
+        """
+        manifest = {'entries': []}
+        for buck in buckets:
+
+            buckpath, keypath = self._get_bucket_and_key(buck)
+            print('Getting bucket {}...'.format(buckpath))
+            bukkit = self.conn.get_bucket(buckpath)
+
+            print('Getting keys for bucket {} in key folder{}...'.format(
+                buckpath, keypath
+            ))
+            for key in bukkit.list(keypath):
+                manifest['entries'].append({
+                    'url': '/'.join(['s3:/', key.bucket.name, key.name]),
+                    'mandatory': mandatory
+                    })
+
+        if path:
+            with open(path, 'w') as fp:
+                json.dump(manifest, fp, sort_keys=True, indent=4)
+
+        if target:
+            self.write_manifest(manifest, target)
+
+        return manifest
+
+    def write_manifest(self, manifest, target=None):
+        """
+        Write a manifest to a given key
+
+        Parameters
+        ----------
+        manifest: dict
+        target: str
+        bucket: Boto Bucket, default None
+            If you already have a bucket, use that one
+        """
+        buckpath, keypath = self._get_bucket_and_key(target)
+        print('Getting bucket {} for writing...'.format(target))
+        bucket = self.conn.get_bucket(buckpath)
+
+        key = Key(bucket, keypath)
+        print("Writing manifest to {}".join(['s3://', key.bucket.name,
+                                             key.name]))
+        key.set_contents_from_string(
+            json.dumps(manifest, sort_keys=True, indent=4)
+        )
